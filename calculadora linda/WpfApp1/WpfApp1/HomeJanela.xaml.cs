4b4aca65220505cec55acac9cf5f180784a1cea7
Maria Eduarda Ribeiro Viana <102558094+mariarviana@users.noreply.github.com>
2022-03-29 21:49:21 -0400

    Add files via upload

---
+++ calculadora linda/WpfApp1/WpfApp1/HomeJanela.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica interna para HomeJanela.xaml
+    /// </summary>
+    public partial class HomeJanela : Window
+    {
+        public HomeJanela()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Adicao_Click(object sender, RoutedEventArgs e)
+        {
+            double n1, n2, resultado;
+
+            n1 = Convert.ToDouble(txt_N1.Text);
+            n2 = Convert.ToDouble(txt_N2.Text);
+
+            resultado = n1 + n2;
+            MessageBox.Show($"Do resultado de {n1} + {n2} é: {resultado}");
+            txt_N1.Clear();
+            txt_N2.Clear();
+        }
+
+        private void bt_Subtracao_Click(object sender, RoutedEventArgs e)
+        {
+            double n1, n2, resultado;
+
+            n1 = Convert.ToDouble(txt_N1.Text);
+            n2 = Convert.ToDouble(txt_N2.Text);
+            resultado = n1 - n2;
+            MessageBox.Show($"Do resultado de {n1} - {n2} é: {resultado}");
+            txt_N1.Clear();
+            txt_N2.Clear();
+        }
+
+        private void bt_Divisao_Click(object sender, RoutedEventArgs e)
+        {
+            double n1, n2, resultado;
+
+            n1 = Convert.ToDouble(txt_N1.Text);
+            n2 = Convert.ToDouble(txt_N2.Text);
+            resultado = n1 / n2;
+            MessageBox.Show($"Do resultado de {n1} / {n2} é: {resultado}");
+            txt_N1.Clear();
+            txt_N2.Clear();
+        }
+
+        private void bt_Multiplicacao_Click(object sender, RoutedEventArgs e)
+        {
+
+            double n1, n2, resultado;
+
+            n1 = Convert.ToDouble(txt_N1.Text);
+            n2 = Convert.ToDouble(txt_N2.Text);
+            resultado = n1 * n2;
+            MessageBox.Show($"Do resultado de {n1} x {n2} é: {resultado}");
+            txt_N1.Clear();
+            txt_N2.Clear();
+        }
+
+        private void Btn_Sair2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ calculadora linda/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_Login_Click(object sender, RoutedEventArgs e)
+        {
+            string nome, senha;
+            nome = txt_Nome.Text;
+            senha = passwordbox_senha.Password.ToString();
+            /*
+             * Jeito de conferir a senha do usuário
+             
+            if (nome.Equals("Maria Eduarda") && senha.Equals("maria"))
+            {
+                MessageBox.Show("Usuário existente");
+                HomeJanela principal = new HomeJanela();
+                principal.ShowDialog();
+            }
+            else
+            { 
+                MessageBox.Show("Usuário inválido");
+            }*/
+            if ((nome != "") && (senha != ""))
+            { 
+                MessageBox.Show("Usuário existente");
+            HomeJanela principal = new HomeJanela();
+            principal.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Usuário inválido");
+            }
+        }
+
+        private void txt_Nome_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txt_Nome.Text = string.Empty;
+        }
+
+        private void txt_Nome_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txt_Nome.Text.Equals(""))
+                txt_Nome.Text = "E-mail ou nome de usuário";
+        }
+
+
+        private void Btn_Sair_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /*
+private void bt_Login_Click_1(object sender, RoutedEventArgs e)
+{
+   HomeJanela principal = new HomeJanela();
+
+   if
+   if (txt_Nome.Text.Equals(""))
+      principal.ShowDialog();
+   else
+       MessageBox.Show("Usuário e senha errado");
+}*/
+    }
+}
